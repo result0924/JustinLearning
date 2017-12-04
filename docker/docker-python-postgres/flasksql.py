086e076938efbc8f711caae1d17248f39293ee5a
jlai <result0924@gmail.com>
2017-12-04 11:46:56 +0800

    Edit web will wait for db to run

--- docker/docker-python-postgres/flasksql.py
+++ docker/docker-python-postgres/flasksql.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://justin:123456@pgdb/justin_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:123456@pgdb/root'
 db = SQLAlchemy(app)
 
 class User(db.Model):
